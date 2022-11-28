653383e60ebf7ae085d10dd73bc51fd243f2442c
d4v1dh <dhalley@null.net>
2022-11-28 16:39:22 +0100

    posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  
   root to: "pages#home"
+
+  devise_scope :user do
+    get 'users', to: 'users/registrations#show', as: 'user'
+  end
+
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
