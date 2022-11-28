8a445c0f860e3db791dcba1178329b49ea359cef
yonghowfung <yhfung940217@gmail.com>
2022-11-28 15:57:08 +0100

    log in and sign out page done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :replies
   has_many :posts
 
+  validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
