114aa89d45d0a1f98890ab2f57be177c9ee19b97
d4v1dh <dhalley@null.net>
2022-11-28 15:37:47 +0100

    post new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
