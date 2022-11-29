0e1a50d7fc4f09e8c7178b70d25af99fa922400e
d4v1dh <dhalley@null.net>
2022-11-29 11:54:44 +0100

    added update with photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,11 @@ class User < ApplicationRecord
   has_many :messages
   has_many :replies
   has_many :posts
+  has_one_attached :photo
 
   # validates :first_name, :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_one_attached :photo
 end
