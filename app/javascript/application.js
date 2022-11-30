7e128bd9fa553a9ca249d169644fdc43af1a6105
Miarinkaja Lovalalaina <lovalalaina@gmail.com>
2022-11-30 14:40:25 +0100

    alert ok but not autohide

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,30 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+// (document).ready( function() {
+//   ('.alert').delay(5000).fadeOut();
+// });
+
+// $ ->
+//       flashCallback = ->
+//         $(".alert").fadeOut()
+//       $(".alert").bind 'click', (ev) =>
+//         $(".alert").fadeOut()
+//       setTimeout flashCallback, 3000
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $("#success-alert").hide();
+//   $("#post").click(function showAlert() {
+//     $("#success-alert").fadeTo(2000, 500).slideUp(500, function() {
+//       $("#success-alert").slideUp(500);
+//     });
+//   });
+// });
