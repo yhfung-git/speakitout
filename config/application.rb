e4c224e5376ddd4e343ea0890a9031be4ba44bb6
d4v1dh <dhalley@null.net>
2022-12-02 10:52:57 +0100

    removed cors

--- config/application.rb
+++ config/application.rb
@@ -26,31 +26,3 @@ module Speakitout
   end
 end
 
-module JobTracker
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
-    end
-  end
-end
-
-
--- config/initializers/cors.rb
+++
@@ -1,4 +0,0 @@
-# gemfile.
-# Author: dhalley <dhalley@mail.com>
-# Created: 2022-11-30 16:37:55
-# Updated: 2022-11-30 16:40:19 by dhalley
