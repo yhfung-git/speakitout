48745a221838900fe9d63a7765bc1fd1d4e467c6
yonghowfung <yhfung940217@gmail.com>
2022-12-04 11:31:22 +0100

    edited alert design

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user = current_user
+    flash[:notice] = "Your article has been successfully created."
     if @article.save
       redirect_to article_path(@article)
     else
@@ -31,6 +32,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
+      flash[:notice] = "Your article has been successfully updated."
       redirect_to article_path(@article)
     else
       render "edit", status: :unprocessable_entity
@@ -42,6 +44,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
+    flash[:notice] = "Your article has been successfully deleted."
     redirect_to articles_path, status: :see_other
   end
 
