7d326d2e6ea2263282b807bfb73039a805ffb90c
yonghowfung <yhfung940217@gmail.com>
2022-12-05 11:01:22 +0100

    wheel updated

---
+++ app/javascript/controllers/spin_wheel_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="spin-wheel"
+export default class extends Controller {
+  static targets = ["container"]
+
+  connect() {
+    console.log("Spin Wheel Connected");
+  }
+
+  spin () {
+    let number = Math.ceil(Math.random() * 1000);
+    this.containerTarget.style.transform = "rotate(" + number + "deg)";
+    number += Math.ceil(Math.random() * 1000);
+  }
+}
+
+
+// let container = document.querySelector(".spin-wheel");
+// let btn = document.getElementById("spin");
+// let number = Math.ceil(Math.random() * 1000);
+
+// btn.onclick = function () {
+// 	container.style.transform = "rotate(" + number + "deg)";
+// 	number += Math.ceil(Math.random() * 1000);
+// }
