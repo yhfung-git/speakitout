334dc34a4edb3d5afe9651e35d9433d98ca2658a
d4v1dh <dhalley@null.net>
2022-12-08 12:25:19 +0100

    forgery protection

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Speakitout
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-     config.action_controller.forgery_protection_origin_check: false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
